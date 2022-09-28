76bbfb5a5b92d3e4678a4f684c0a0983c762aec0
haripandey91 <pandey_hari2@hotmail.com>
2022-09-28 15:32:44 +0300

    getting started to request and response

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const app = express();
+
+app.get("/", (req, res) => {
+  response.send("Hello");
+});
+
+app.listen(3000, () => {console.log("Server started at port 3000");});
